c9a5bb7f94c802a1f6aad701c02315fefad8491c
LiangT <liang.tang@asb.co.nz>
2022-06-20 09:05:42 +1200

    MutableList ArrayList

--- coroutines/src/test/kotlin/BasicTest.kt
+++ coroutines/src/test/kotlin/BasicTest.kt
@@ -15,7 +15,25 @@ class BasicTest {
     @Test
     fun testSort() {
         println(listOf("Aa d", "Ac g", "aA", "aR", "Ab")
-                .sortedBy { it })
+            .sortedBy { it })
 
+    }
+
+    data class TestClass(
+        val s: String
+    )
+
+    data class MutableListInside(
+        val list: MutableList<TestClass>
+    )
+
+    // After copy MutableList will change to ArrayList
+    @Test
+    fun testMutableListCopy() {
+        val testData = MutableListInside(mutableListOf(TestClass("abc")))
+
+        val copy = testData.copy()
+
+        println(copy)
     }
 }
